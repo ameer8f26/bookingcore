c2a7bb4233db048ca2f3d466e18ab27388b3b888
ameer8f26 <hamzait2017@gmail.com>
2021-12-07 14:37:33 +0500

    check home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,6 +29,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        dd(asset('libs/bootstrap/css/bootstrap.css'));
         $home_page_id = setting_item('home_page_id');
         if($home_page_id && $page = Page::where("id",$home_page_id)->where("status","publish")->first())
         {
